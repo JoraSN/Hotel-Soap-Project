553436251f22e6c08e9a39d262adf5da11040509
JoraSN <89937640+JoraSN@users.noreply.github.com>
2023-08-14 13:40:46 -0700

    Create mixer.ino

---
+++ mixer.ino
@@ -0,0 +1,18 @@
+#include <ESP32Servo.h>
+ 
+Servo myservo; 
+ 
+int servoPin = 2;
+ 
+void setup() {
+  myservo.setPeriodHertz(50); 
+  myservo.attach(servoPin);
+}
+ 
+void loop() {
+   for(int pos = 0; pos < 360; pos += 1) {
+ myservo.write(pos); 
+ delay(15);
+ }
+
+}
