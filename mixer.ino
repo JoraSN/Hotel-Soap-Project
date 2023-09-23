7e88a96b6f0751a147a0a561a35cec7de9640077
JoraSN <89937640+JoraSN@users.noreply.github.com>
2023-09-23 16:48:57 -0700

    Update mixer.ino

--- mixer.ino
+++ mixer.ino
@@ -1,8 +1,10 @@
 #include <ESP32Servo.h>
  
 Servo myservo; 
- 
+
+const int custom = 10000; //enter custom duration in ms
 const int servoPin = 2;
+int cond = 1;
  
 void setup() {
   myservo.setPeriodHertz(50); 
@@ -10,9 +12,12 @@ void setup() {
 }
  
 void loop() {
+ while(cond){
    for(int pos = 0; pos < 360; pos += 1) {
  myservo.write(pos); 
  delay(15);
  }
-
+ }
+ delay(custom);
+cond = 0;
 }
